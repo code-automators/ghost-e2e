bef200a30d7bc58b63d32a94006e96e1e2ad8c7e
LuisaTorresMon <lj.torresm1@uniandes.edu.co>
2023-05-05 20:35:43 -0500

    Actualización del escenario 2 - Editar un post

--- src/cypress_tests/cypress/e2e/pages/postPage.cy.js
+++ src/cypress_tests/cypress/e2e/pages/postPage.cy.js
@@ -7,10 +7,15 @@ class PostPage {
     }
   }
 
-  editPost() {
-    cy.contains("Posts").click(); // busca y hace click en el enlace "Posts"
-    cy.get("a.gh-list-data.gh-post-list-title").first().click(); // selecciona el primer post y hace click en su título
-    cy.get("textarea.gh-editor-title").clear().type("Nuevo título del post");
+  selectPost() {
+    cy.contains("Posts").click();
+    return cy.get("a.gh-list-data.gh-post-list-title").first();
+  }
+
+  editPost(postTitle) {
+    cy.contains("Posts").click();
+    cy.get("a.gh-list-data.gh-post-list-title").first().click();
+    cy.get("textarea.gh-editor-title").clear().type(postTitle);
     cy.get("div.gh-publishmenu-trigger").click();
     cy.get("button.gh-btn-blue.gh-publishmenu-button").click();
     return cy.get(".gh-notification-content");
--- src/cypress_tests/cypress/e2e/scenarios/post_2.scenario.cy.js
+++ src/cypress_tests/cypress/e2e/scenarios/post_2.scenario.cy.js
@@ -1,14 +1,40 @@
 import { SignInPage } from "../pages/signinPage.cy";
 
-describe("Editar un post existente y actualizarlo", () => {
-  it("Caso de prueba 1: Seleccionar un post existente para editar", () => {
+describe('Editar un post existente y actualizar su titulo', () => {
+  const postTitle = "Nuevo título del post";
+
+  // Caso de prueba 1: Seleccionar un post existente para editar
+  it('Seleccionar un post existente para editar', () => {
     // Given user is logged in
     let signinPage = new SignInPage();
     let homePage = signinPage.login();
-    // When user edit a post
+    // when user lists the posts
     let postPage = homePage.goToPost();
-    postPage.editPost();
-    // Then post is updated
-    postPage.editPost().should("contain", "Updated");
+    postPage.selectPost();
+    //Then post is selected
+    postPage.selectPost().should('not.be.empty');
+  });
+
+  // Caso de prueba 2: Realizar cambios en el post
+  it('Realizar cambios en el post', () => {
+    let signinPage = new SignInPage();
+    let homePage = signinPage.login();
+    // when user lists the posts
+    let postPage = homePage.goToPost();
+    postPage.editPost(postTitle);
+    //Then post is Updated
+    postPage.editPost(postTitle).should("contain", "Updated");
+  });
+
+  // Caso de prueba 3: Validar el post editado
+  it('Validar el post editado', () => {
+    // Given user is logged in
+    let signinPage = new SignInPage();
+    let homePage = signinPage.login();
+    // when user lists the posts
+    let postPage = homePage.goToPost();
+    postPage.selectPost();
+    //Then post is edited
+    postPage.selectPost().should('contain', postTitle);
   });
 });
