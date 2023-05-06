5b0cb2ad02a1642f7e6cb9e9fb94bf221f66b4bc
Juan Correa <juanpablocorreapuerta@gmail.com>
2023-05-06 16:11:44 -0600

    Code style improvement

--- src/cypress_tests/cypress/e2e/scenarios/Scenario1.cy.js
+++ src/cypress_tests/cypress/e2e/scenarios/Scenario1.cy.js
@@ -2,23 +2,16 @@ import { SignInPage } from "../pages/signinPage.cy";
 import config from "./assets/config.json";
 
 
-describe("Crear un post con tag y publicarlo", () => {
-  const TITLE = "LA GENTE DEL PUEBLO";
-  const CONTENT = "Contenido del nuevo post";
-  const TAG = "Tag de prueba";
-
-  // Escenario 1: crear un post con tag y publicarlo
+describe("Create a new post with a tag and publish it", () => {
   it("Scenario 1", () => {
     // Given user is logged in
     let signinPage = new SignInPage();
     let homePage = signinPage.login(config.user, config.password);
     // When user view the posts
     let postList = homePage.goToPostList();
-    // Then create a post
+    // Then create, fill and save a post
     let newPostDetail = postList.createPost();
-    // And fill and save it
-    newPostDetail.fillNewPost(TITLE, CONTENT, TAG);
-
+    newPostDetail.fillNewPost(config.new_title, config.new_content, config.new_tag_name);
     homePage.goToPostList();
     // Then post was created
     postList.selectPost().should("contain", TITLE);
