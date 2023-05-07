a51bb8e11b870f466dc0e4f8cdc6fd97b79e61eb
Juan Correa <juanpablocorreapuerta@gmail.com>
2023-05-07 01:13:28 -0600

    Finished scenario 17

--- src/cypress_tests/cypress/e2e/scenarios/Scenario17.cy.js
+++ src/cypress_tests/cypress/e2e/scenarios/Scenario17.cy.js
@@ -1,22 +1,21 @@
 import { SignInPage } from "../pages/signinPage.cy";
 import  config  from "./assets/config.json";
 
-describe("Change user password", () => {
+describe("Change user password and login using these new password.", () => {
 
     it("Scenario 17", () => {
         // Given user is logged in
         let signinPage = new SignInPage();
         let homePage = signinPage.login(config.user, config.password);
-        // When the user wants to change their user and password
+        // When the user wants to change their password
         let profilePage = homePage.goToProfile();
-        // The user changes their credentials
-        profilePage.changeCredentials(config.new_user, config.new_password, config.password)
+        // The user changes their password
+        profilePage.changeCredentials(config.user, config.new_password, config.password);
         // The user logs out
-        signinPage = homePage.logout()
-        // Then if the user tries to log in with old credentials, Ghost should show an error
-        homePage = signinPage.login(config.new_user, config.new_password);
-        // The user should log in successfully
-        homePage.getUrl().should('eq', 'http://localhost:2368/ghost/#/site')
+        signinPage = homePage.logout();
+        homePage = signinPage.login(config.user, config.new_password);
+        // Then the user log in successfully with new password
+        homePage.getUrl().should("eq", "http://localhost:2368/ghost/#/site");
     })
 
 })
