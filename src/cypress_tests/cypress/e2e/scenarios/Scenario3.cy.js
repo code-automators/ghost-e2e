4c64a64b709c77e9acf7770bb46f4aa198479458
Juan Alegría <j.alegria@uniandes.edu.co>
2023-05-07 10:28:18 -0500

    Test(Cypress): add scenario 3

---
+++ src/cypress_tests/cypress/e2e/scenarios/Scenario3.cy.js
@@ -0,0 +1,19 @@
+import { SignInPage } from "../pages/signinPage.cy";
+import config from "./assets/config.json";
+
+
+describe("Create a new tag with an invalid description", () => {
+    it("Scenario 3", () => {
+        // Given user is logged in
+        let signinPage = new SignInPage();
+        let homePage = signinPage.login(config.user, config.password);
+        // When the user goes to the tag list
+        let tagsListPage = homePage.goToTagsList();
+        // And the user goes to create tag and fills out the details with a too large description
+        let newTag = tagsListPage.goToCreateTag();
+        newTag.createNewTag(config.new_tag_name, config.new_tag_slug, config.image_path, "a".repeat(501));
+        // Then retry and error message appears. Tag is not created.
+        newTag.verifySaveBtn().should("contain", "Retry");
+        newTag.getCreationResponse().should("contain", "Description cannot be longer than 500 characters.");
+    });
+});
