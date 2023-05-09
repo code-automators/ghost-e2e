fefcb154ce767d0ce31e0ef931f7e5ce81be54f5
ElReyZero <juan.romero1201@gmail.com>
2023-05-09 16:41:23 -0500

    Fix: Solve some scenario bugs

--- src/report_script/script.js
+++ src/report_script/script.js
@@ -65,4 +65,51 @@ async function executeTest() {
     console.log("Execution finished. Check the report under the results folder")
     return resultInfo;
 }
+
+
+function browser(b, info){
+    return `<div class=" browser" id="test0">
+    <div class=" btitle">
+        <h2>Browser: ${b}</h2>
+        <p>Data: ${JSON.stringify(info)}</p>
+    </div>
+    <div class="imgline">
+      <div class="imgcontainer">
+        <span class="imgname">Reference</span>
+        <img class="img2" src="before-${b}.png" id="refImage" label="Reference">
+      </div>
+      <div class="imgcontainer">
+        <span class="imgname">Test</span>
+        <img class="img2" src="after-${b}.png" id="testImage" label="Test">
+      </div>
+    </div>
+    <div class="imgline">
+      <div class="imgcontainer">
+        <span class="imgname">Diff</span>
+        <img class="imgfull" src="./compare-${b}.png" id="diffImage" label="Diff">
+      </div>
+    </div>
+  </div>`
+}
+
+
+function createReport(datetime, resInfo){
+    return `
+    <html>
+        <head>
+            <title> VRT Report </title>
+            <link href="index.css" type="text/css" rel="stylesheet">
+        </head>
+        <body>
+            <h1>Report for
+                 <a href="${config.url}"> ${config.url}</a>
+            </h1>
+            <p>Executed: ${datetime}</p>
+            <div id="visualizer">
+                ${config.browsers.map(b=>browser(b, resInfo[b]))}
+            </div>
+        </body>
+    </html>`
+}
+
 (async () => console.log(await executeTest()))();
