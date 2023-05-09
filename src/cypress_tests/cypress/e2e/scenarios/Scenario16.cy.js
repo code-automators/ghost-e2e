79a8e8c6c0522faf770fa2ed9d7916aa12cdc7cc
ElReyZero <juan.romero1201@gmail.com>
2023-05-09 07:59:13 -0500

    Feat: Add take cypress screenshot to Scenarios 4, 8, 12, 16 and 20

--- src/cypress_tests/cypress/e2e/scenarios/Scenario16.cy.js
+++ src/cypress_tests/cypress/e2e/scenarios/Scenario16.cy.js
@@ -1,5 +1,6 @@
 import { SignInPage } from "../pages/signinPage.cy";
 import  config  from "./assets/config.json";
+import { takeCypressScreenshot } from "./utils/takeScreenshot";
 
 describe("Scenario 16", () => {
 
@@ -12,19 +13,26 @@ describe("Scenario 16", () => {
         // Given user is logged in
         let signinPage = new SignInPage();
         let homePage = signinPage.login(config.user, config.password);
+        takeCypressScreenshot("Scenario16", "login")
         // When the user wants to create a new tag
         let tagsListPage = homePage.goToTagsList();
+        takeCypressScreenshot("Scenario16", "goToTagsList")
         // And the user goes to create tag and fills out the details
         let newTag = tagsListPage.goToCreateTag();
+        takeCypressScreenshot("Scenario16", "goToCreateTag")
         newTag.createNewTag(uniqueTagName, uniqueTagSlug, config.image_path);
+        takeCypressScreenshot("Scenario16", "createNewTag")
         // And once it is created, the user goes to the post list and selects a random post
         let postsPage = homePage.goToPostList();
+        takeCypressScreenshot("Scenario16", "goToPostList")
         let postDetails = postsPage.selectRandomPost();
         // And the user adds the new tag to the post
         postDetails.addTagToPost(uniqueTagName);
+        takeCypressScreenshot("Scenario16", "addTagToPost")
         // Then the post with the new tag should be displayed in the post list
         let filteredPosts = homePage.goToPostListFilteredByTag(uniqueTagSlug);
         filteredPosts.selectPost().should('exist');
+        takeCypressScreenshot("Scenario16", "checkPostWithNewTag")
     })
 
 })
--- src/cypress_tests/cypress/e2e/scenarios/Scenario20.cy.js
+++ src/cypress_tests/cypress/e2e/scenarios/Scenario20.cy.js
@@ -1,6 +1,7 @@
 import { HomePage } from "../pages/homePage.cy";
 import { SignInPage } from "../pages/signinPage.cy";
 import  config  from "./assets/config.json";
+import { takeCypressScreenshot } from "./utils/takeScreenshot";
 
 describe("Scenario 20", () => {
 
@@ -8,13 +9,17 @@ describe("Scenario 20", () => {
         // Given user is logged in
         let signinPage = new SignInPage();
         let homePage = signinPage.login(config.user, config.password);
+        takeCypressScreenshot("Scenario20", "login")
         // When the user wants to change the blog's settings
         let settingsPage = homePage.goToGeneralSettings();
+        takeCypressScreenshot("Scenario20", "goToGeneralSettings")
         // And the user changes the settings and makes the blog private
         settingsPage.changeSettingsAndMakePrivate(config.new_site_name);
+        takeCypressScreenshot("Scenario20", "changeSettingsAndMakePrivate")
         // Then when the user goes to the main page, it should be private
         let mainPage = homePage.goToMainPageSite();
         mainPage.checkIfSiteIsPrivate().should('contain', 'This site is private');
+        takeCypressScreenshot("Scenario20", "checkIfSiteIsPrivate")
     })
 
     after(()=> {
