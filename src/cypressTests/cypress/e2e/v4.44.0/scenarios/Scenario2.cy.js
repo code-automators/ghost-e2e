d846d0ee698b68ce82436f6d59459d7a408744c4
LuisaTorresMon <lj.torresm1@uniandes.edu.co>
2023-05-10 20:53:29 -0500

    Feature: modify scenarios 2, 10 and 18 for ghost version 4.44

---
+++ src/cypressTests/cypress/e2e/v4.44.0/scenarios/Scenario2.cy.js
@@ -0,0 +1,27 @@
+import { SignInPage } from "./../pages/signinPage.cy";
+import { takeCypressScreenshot } from "./../utils/takeScreenshot";
+import config from "./../assets/config.json";
+
+
+describe("Escenario 2", () => {
+
+    it("Editar un post existente actualizando su título", () => {
+        // Given user is logged in
+        let signinPage = new SignInPage();
+        let homePage = signinPage.login(config.user, config.password);
+        takeCypressScreenshot("Scenario2", "login");
+        //when the user wants to edit a post
+        let postsPage = homePage.goToPostList();
+        takeCypressScreenshot("Scenario2", "goToPostList");
+        // And the user select random post
+        let postDetails = postsPage.selectRandomPost();
+        takeCypressScreenshot("Scenario2", "selectRandomPost");
+        // And the user edit post
+        postDetails.editPost(config.new_title);
+        takeCypressScreenshot("Scenario2", "editPost");
+        homePage.goToPostList();
+        // Then the edited post is displayed in the post list with the new title
+        postsPage.getListPosts().should('contain', config.new_title);
+        takeCypressScreenshot("Scenario2", "checkPostEdited")
+    })
+})
