dd13db0e61c863ff67c2fa32c982d85a689f447b
ElReyZero <juan.romero1201@gmail.com>
2023-05-11 22:33:38 -0500

    CI: Debug

--- src/cypressTests/cypress/e2e/v3.41.1/scenarios/Scenario09.cy.js
+++ src/cypressTests/cypress/e2e/v3.41.1/scenarios/Scenario09.cy.js
@@ -1,7 +1,7 @@
 import { SignInPage } from "./../pages/signinPage.cy";
 import config from "./../assets/config.json";
 
-describe("Create a post with an image", () => {
+describe("Create a page with an image", () => {
 
     it("Scenario09", () => {
         // Given user is logged in
@@ -11,9 +11,8 @@ describe("Create a post with an image", () => {
         let pageListPage = homePage.goToPageList();
         // The user goes to the page list and selects a random page
         let createPage = pageListPage.goToCreatePage();
+        // The user creates the page and uploads an image
         let newPage = createPage.createNewImagedPage(config.new_page_name, config.image_path);
-        // The user edits the page and uploads an image
-        // newPage.uploadNewImage(config.image_path);
         // Then the edited page should be updated
         newPage.getList().should("contain", config.new_page_name);
     });
