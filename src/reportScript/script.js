28880ed8c2954960659b97faf9623da1b1fe0742
Juan Alegría <j.alegria@uniandes.edu.co>
2023-05-12 12:14:28 -0500

    CI: add list reports html

--- src/reportScript/script.js
+++ src/reportScript/script.js
@@ -68,13 +68,10 @@ function getUserInput(message) {
 
 function getFormattedDate() {
   let date = new Date();
-  return (
-    date.getFullYear() +
-    "-" +
-    (date.getMonth() + 1) +
-    "-" +
-    date.getDate()
-  );
+  let year = date.getFullYear();
+  let month = (date.getMonth() + 1).toString().padStart(2, '0');
+  let day = date.getDate().toString().padStart(2, '0');
+  return year + "-" + month + "-" + day;
 }
 
 function getDateString() {
