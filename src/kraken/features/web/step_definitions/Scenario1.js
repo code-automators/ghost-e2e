2c029155e7266711e0929dd1b8c75de9cdf0559a
Juan Correa <juanpablocorreapuerta@gmail.com>
2023-05-13 00:31:17 -0600

    Added Kraken screenshots

--- src/kraken/features/web/step_definitions/Scenario1.js
+++ src/kraken/features/web/step_definitions/Scenario1.js
@@ -4,26 +4,30 @@ const { clickOnPostsTab } = require("./page_objects/admin_menu.js");
 const { createPost } = require("./page_objects/post_details.js");
 const { clickOnNewPost } = require("./page_objects/posts_list.js");
 const { checkNewPostEdited } = require("./page_objects/post_details.js");
+const { takeKrakenScreenshot } = require("./utils/takeScreenshot.js");
 
 
 Given("I login with {kraken-string} and {kraken-string} - Scenario one", async function (email, password) {
-    await login(this.driver, email, password)
+    await login(this.driver, email, password);
+    await takeKrakenScreenshot(this.driver, "Scenario1", "login");
 });
 
 When("I click on the Posts tab - Scenario one", async function () {
-    await clickOnPostsTab(this.driver)
+    await clickOnPostsTab(this.driver);
+    await takeKrakenScreenshot(this.driver, "Scenario1", "clickOnPostsTab");
 });
 
 When("I click on 'New Post' Button", async function () {
     await clickOnNewPost(this.driver);
+    await takeKrakenScreenshot(this.driver, "Scenario1", "clickOnNewPost");
 });
 
-When("I create a new post with {kraken-string}, {kraken-string} and tag {kraken-string}",
-    async function(title, content, tagname) {
-        await createPost(this.driver, title, content, {tagname});
-    }
-);
+When("I create a new post with {kraken-string}, {kraken-string} and tag {kraken-string}", async function(title, content, tagname) {
+    await createPost(this.driver, title, content, {tagname});
+    await takeKrakenScreenshot(this.driver, "Scenario1", "createPost");
+});
 
 Then("I should see the new post created called {kraken-string}", async function (page_title) {
     await checkNewPostEdited(this.driver, page_title);
+    await takeKrakenScreenshot(this.driver, "Scenario1", "checkNewPostEdited");
 });
--- src/kraken/features/web/step_definitions/Scenario17.js
+++ src/kraken/features/web/step_definitions/Scenario17.js
@@ -6,11 +6,14 @@ const { clickOnProfileMenu } = require("./page_objects/admin_menu.js");
 const { checkAuthorOptions } = require("./page_objects/author_page.js");
 const { changeAuthorInfo } = require("./page_objects/profile_page.js");
 
+const { takeKrakenScreenshot } = require("./utils/takeScreenshot.js");
+
 
 Given(
     "I login with {kraken-string} and {kraken-string} - Scenario seventeen",
     async function (email, password) {
-        await login(this.driver, email, password)
+        await login(this.driver, email, password);
+        await takeKrakenScreenshot(this.driver, "Scenario17", "login");
     }
 );
 
@@ -18,6 +21,7 @@ When(
     "I click on the profile dropdown and access my profile - Scenario seventeen",
     async function () {
         await clickOnProfileMenu(this.driver);
+        await takeKrakenScreenshot(this.driver, "Scenario17", "goToProfile");
     }
 );
 
@@ -31,18 +35,21 @@ When(
             facebook,
             twitter
         );
+        await takeKrakenScreenshot(this.driver, "Scenario17", "changeAuthorInfo");
     }
 );
 
 Then("I navigate to authors page with slug {kraken-string}",
     async function (slug) {
         await this.driver.url(`http://localhost:2368/author/${slug}`);
+        await takeKrakenScreenshot(this.driver, "Scenario17", "goToAuthorPage");
     }
 );
-
+    
 Then(
     "I should see the author options",
     async function () {
         await checkAuthorOptions(this.driver);
+        await takeKrakenScreenshot(this.driver, "Scenario17", "checkAuthorOptions");
     }
 );
