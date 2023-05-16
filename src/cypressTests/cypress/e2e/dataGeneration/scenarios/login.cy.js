090d25d45e9bda783195d23c4dfc11a1b91fbecc
ElReyZero <juan.romero1201@gmail.com>
2023-05-16 09:43:27 -0500

    Feat: Add scenario 3

--- src/cypressTests/cypress/e2e/dataGeneration/scenarios/login.cy.js
+++ src/cypressTests/cypress/e2e/dataGeneration/scenarios/login.cy.js
@@ -5,14 +5,12 @@ import { faker } from '@faker-js/faker';
 
 describe("Login Scenarios", () => {
     it("Scenario 1 - A Priori", () => {
-        // Given user is logged in
         let signinPage = new SignInPage();
         signinPage.login(data.scenario1.email, data.scenario1.password);
         signinPage.checkForNonExistentUser().should("contain", "There is no user with that email address.")
     });
 
     it("Scenario 1 - Pseudo Random", () => {
-        // Given user is logged in
         let signinPage = new SignInPage();
         cy.request(`https://my.api.mockaroo.com/login.json?key=${config.mockarooKey}`)
         .then((response) => {
@@ -23,23 +21,42 @@ describe("Login Scenarios", () => {
     });
 
     it("Scenario 1 - Random", () => {
-        // Given user is logged in
         let signinPage = new SignInPage();
         signinPage.login(faker.internet.email(), faker.internet.password());
         signinPage.checkForNonExistentUser().should("contain", "There is no user with that email address.")
     });
 
     it("Scenario 2 - A Priori", () => {
-        // Given user is logged in
         let signinPage = new SignInPage();
         signinPage.login(config.user, data.scenario2.password);
         signinPage.checkForIncorrectPassword().should("contain", "Your password is incorrect.")
     });
 
     it("Scenario 2 - Random", () => {
-        // Given user is logged in
         let signinPage = new SignInPage();
         signinPage.login(config.user, faker.internet.password());
         signinPage.checkForIncorrectPassword().should("contain", "Your password is incorrect.")
+    });
+
+    it("Scenario 3 - A Priori", () => {
+        let signinPage = new SignInPage();
+        signinPage.login(data.scenario3.email.repeat(25), data.scenario3.password);
+        signinPage.checkForErrorData().should("contain", "Please fill out the form to sign in.")
+    });
+
+    it("Scenario 3 - Pseudo Random", () => {
+        let signinPage = new SignInPage();
+        cy.request(`https://my.api.mockaroo.com/login.json?key=${config.mockarooKey}`)
+        .then((response) => {
+            console.log(response.body);
+            signinPage.login(response.body.email.repeat(25), response.body.password);
+            signinPage.checkForErrorData().should("contain", "Please fill out the form to sign in.")
+        })
+    });
+
+    it("Scenario 3 - Random", () => {
+        let signinPage = new SignInPage();
+        signinPage.login(faker.internet.email().repeat(25), faker.internet.password());
+        signinPage.checkForErrorData().should("contain", "Please fill out the form to sign in.")
     });
 });
