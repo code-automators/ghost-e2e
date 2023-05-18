2a33f65fc44dd5d2cc7c7696004cf6725b28de7d
ElReyZero <juan.romero1201@gmail.com>
2023-05-18 11:21:20 -0500

    Feat: Add scenarios with invalid profile

--- src/cypressTests/cypress/e2e/dataGeneration/scenarios/profile.cy.js
+++ src/cypressTests/cypress/e2e/dataGeneration/scenarios/profile.cy.js
@@ -95,6 +95,88 @@ describe("Profile Scenarios", () => {
         saveCurrentTestCredentials("Edit profile with valid info", scenarioPassword);
     });
 
+    it("[A priori] Scenario 19: Edit profile with invalid info", () => {
+        // Given user is logged in
+        let signinPage = new SignInPage();
+        let homePage = signinPage.login(config.user, config.password);
+        // When the user wants to change the slug and social media info
+        let profilePage = homePage.goToProfile();
+        // And the user changes their profile
+        profilePage.changeSlugAndSocialMedia(
+            data.scenario19.slug,
+            data.scenario19.website,
+            data.scenario19.facebook,
+            data.scenario19.twitter
+        );
+
+        profilePage.changeCredentials(
+            data.scenario19.newEmail,
+            data.scenario19.newPassword,
+            config.password,
+            false
+        )
+        profilePage.getInvalidEmail().should('exist')
+        profilePage.getInvalidSocialMedia().should('exist')
+        profilePage.getInvalidPassword().should('exist')
+        saveCurrentTestCredentials("Edit profile with invalid info", data.scenario16.newPassword);
+    });
+
+    it("[Pseudo Random] Scenario 20: Edit profile with invalid info", () => {
+        // Given user is logged in
+        let signinPage = new SignInPage();
+        let homePage = signinPage.login(config.user, config.password);
+        // When the user wants to change the slug and social media info
+        let profilePage = homePage.goToProfile();
+        // And the user changes their profile
+        cy.request(`https://my.api.mockaroo.com/invalidProfile.json?key=${config.mockarooKey}`)
+            .then((response) => {
+                profilePage.changeSlugAndSocialMedia(
+                    response.body.slug,
+                    response.body.website,
+                    response.body.facebook,
+                    response.body.twitter
+                );
+                profilePage.changeCredentials(
+                    response.body.newEmail,
+                    response.body.newPassword,
+                    config.password,
+                    false
+                )
+                profilePage.getInvalidEmail().should('exist')
+                profilePage.getInvalidSocialMedia().should('exist')
+                profilePage.getInvalidPassword().should('exist')
+                saveCurrentTestCredentials("Edit profile with invalid info", response.body.newPassword);
+            })
+    });
+
+    it("[Random] Scenario 21: Edit profile with invalid info", () => {
+        // Given user is logged in
+        let signinPage = new SignInPage();
+        let homePage = signinPage.login(config.user, config.password);
+        // When the user wants to change the slug and social media info
+        let profilePage = homePage.goToProfile();
+        // And the user changes their profile
+        profilePage.changeSlugAndSocialMedia(
+            faker.lorem.slug(),
+            faker.lorem.paragraph(4),
+            faker.string.symbol(20),
+            faker.lorem.paragraph(5).concat(faker.string.symbol(5))
+        );
+
+        let scenarioEmail = faker.lorem.paragraph(2);
+        let scenarioPassword = faker.internet.password({length:5});
+        profilePage.changeCredentials(
+            scenarioEmail,
+            scenarioPassword,
+            config.password,
+            false
+        )
+        profilePage.getInvalidEmail().should('exist')
+        profilePage.getInvalidSocialMedia().should('exist')
+        profilePage.getInvalidPassword().should('exist')
+        saveCurrentTestCredentials("Edit profile with invalid info", scenarioPassword);
+    });
+
 
     afterEach(() => {
         if (currentTest.currentScenario.includes("Edit profile with valid info")) {
